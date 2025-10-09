df23a5154542bbc7187542787261a9fe1ec0d4d0
Ronnarit00000 <house2541@gmail.com>
2025-10-09 10:20:34 +0700

    create new carousel component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,18 +1,16 @@
 import Carousel from "../utils/Carousel"
 
 export default function Work() {
-  const soure = [
-    "/assets/image-slide-1.jpg",
-    "/assets/image-slide-2.jpg",
-    "/assets/image-slide-3.jpg",
-    "/assets/image-slide-4.jpg",
-    "/assets/image-slide-5.jpg",
-  ]
-
   return (
     <section className="work">
       <h2 className="text-preset-2">My Work</h2>
-      <Carousel soure={soure}/>
+      <Carousel>
+        <img src="/assets/image-slide-1.jpg" alt="image -slide" />
+        <img src="/assets/image-slide-2.jpg" alt="image -slide" />
+        <img src="/assets/image-slide-3.jpg" alt="image -slide" />
+        <img src="/assets/image-slide-4.jpg" alt="image -slide" />
+        <img src="/assets/image-slide-5.jpg" alt="image -slide" />
+      </Carousel >
     </section>
   )
 }
